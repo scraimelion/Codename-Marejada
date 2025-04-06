2631fe1630127966b6587a48b612911fe530fcd3
scraimelion <eiwa5000@gmail.com>
2025-04-06 19:07:58 +0200

    Añadido proyecto de Unity (proyecto incluye prototipo de Unity de FPS)

---
+++ Marejada/Assets/FPS/Scripts/Gameplay/PlagerDungeonController.cs
@@ -0,0 +1,100 @@
+using Unity.FPS.Game;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unity.FPS.Gameplay {
+
+    [RequireComponent(typeof(CharacterController), typeof(PlayerInputHandler), typeof(AudioSource))]
+    public class PlagerDungeonController : MonoBehaviour {
+
+        [Header("References")] [Tooltip("Reference to the main camera used for the player")]
+        public Camera PlayerCamera;
+
+        [Tooltip("Audio source for footsteps, jump, etc...")]
+        public AudioSource AudioSource;
+
+        [Header("Movement")] [Tooltip("Max movement speed when grounded (when not sprinting)")]
+        public float tileMovement = 10f;
+
+        [Tooltip("Sound played for footsteps")]
+        public AudioClip FootstepSfx;
+
+        public Vector3 CharacterVelocity { get; set; }
+
+        [SerializeField]
+        float test;
+
+        PlayerInputHandler m_InputHandler;
+        CharacterController m_Controller;
+        Actor m_Actor;
+        Vector3 m_CharacterVelocity;
+        float m_CameraVerticalAngle = 0f;
+        float m_FootstepDistanceCounter;
+        float m_TargetCharacterHeight;
+
+        void Awake()
+        {
+            ActorsManager actorsManager = FindFirstObjectByType<ActorsManager>();
+            if (actorsManager != null)
+                actorsManager.SetPlayer(gameObject);
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start() {
+            // fetch components on the same gameObject
+            m_Controller = GetComponent<CharacterController>();
+            DebugUtility.HandleErrorIfNullGetComponent<CharacterController, PlayerCharacterController>(m_Controller,
+                this, gameObject);
+
+            m_InputHandler = GetComponent<PlayerInputHandler>();
+            DebugUtility.HandleErrorIfNullGetComponent<PlayerInputHandler, PlayerCharacterController>(m_InputHandler,
+                this, gameObject);
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            HandleCharacterMovement();
+        }
+
+        void HandleCharacterMovement()
+        {
+            // converts move input to a worldspace vector based on our character's transform orientation
+            Vector3 worldspaceMoveInput = transform.TransformVector(m_InputHandler.GetMoveInput());
+
+            if (worldspaceMoveInput.x != 0) {
+                CharacterVelocity = Vector3.right * worldspaceMoveInput.x * tileMovement;
+            }
+            else {
+                CharacterVelocity = new Vector3();
+            }
+
+            Vector3 capsuleBottomBeforeMove = GetCapsuleBottomHemisphere();
+            Vector3 capsuleTopBeforeMove = GetCapsuleTopHemisphere(m_Controller.height);
+            m_Controller.Move(CharacterVelocity * Time.deltaTime);
+
+            // detect obstructions to adjust velocity accordingly
+            if (Physics.CapsuleCast(capsuleBottomBeforeMove, capsuleTopBeforeMove, m_Controller.radius,
+                CharacterVelocity.normalized, out RaycastHit hit, CharacterVelocity.magnitude * Time.deltaTime, -1,
+                QueryTriggerInteraction.Ignore))
+            {
+                CharacterVelocity = Vector3.ProjectOnPlane(CharacterVelocity, hit.normal);
+            }
+        }
+
+        // Gets the center point of the bottom hemisphere of the character controller capsule    
+        Vector3 GetCapsuleBottomHemisphere()
+        {
+            return transform.position + (transform.up * m_Controller.radius);
+        }
+
+        // Gets the center point of the top hemisphere of the character controller capsule    
+        Vector3 GetCapsuleTopHemisphere(float atHeight)
+        {
+            return transform.position + (transform.up * (atHeight - m_Controller.radius));
+        }
+
+    }
+
+}
+
