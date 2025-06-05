e099f8d403737e65f98c97d1db5f4fbc8324f1fc
Patricia Soto <patriciasm.12@gmail.com>
2025-06-05 11:20:18 +0200

    Introdef

--- Marejada/Assets/Scripts/DialogSystem/DialogueTrigger.cs
+++ Marejada/Assets/Scripts/DialogSystem/DialogueTrigger.cs
@@ -6,6 +6,15 @@ namespace DialogueSystem
 {
     public class DialogueTrigger : MonoBehaviour
     {
+    
+    if (DialogueManager.Instance != null)
+{
+    DialogueManager.Instance.StartDialogue(dialogue);
+}
+else
+{
+    Debug.LogError("❌ DialogueManager no encontrado.");
+}
         [SerializeField] private DialogueRoundSO dialogue;
         [SerializeField] private UnityEvent DialogEvents;
 
